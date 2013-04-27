99e363f6180bb32d3856792832dcdeaa81fc735a
myuxie <myu.xie@berkeley.edu>
2013-04-26 18:18:22 -0700

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,55 @@
+public class Edge {
+    private Object vertex1;
+    private Object vertex2;
+    private int weight;
+
+    public Edge (Object start, Object end, int weight) {
+        vertex1 = start;
+        vertex2 = end;
+        weight = this.weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    //public int hashCode() {
+    //return 
+    //}
+
+    //public boolean equals() {
+        
+    //}
+
+    /**
+   * hashCode() returns a hashCode equal to the sum of the hashCodes of each
+   * of the two objects of the pair, so that the order of the objects will
+   * not affect the hashCode.  Self-edges are treated differently:  we don't
+   * add an object's hashCode to itself, since the result would always be even.
+   * We add one to the hashCode so that a self-edge will not collide with the
+   * object itself if vertices and edges are stored in the same hash table.
+   */
+  public int hashCode() {
+    if (vertex1.equals(vertex2)) {
+      return vertex1.hashCode() + 1;
+    } else {
+      return vertex1.hashCode() + vertex2.hashCode();
+    }
+  }
+
+  /**
+   * equals() returns true if this VertexPair represents the same unordered
+   * pair of objects as the parameter "o".  The order of the pair does not
+   * affect the equality test, so (u, v) is found to be equal to (v, u).
+   */
+  public boolean equals(Object o) {
+    if (o instanceof Edge) {
+      return ((vertex1.equals(((Edge) o).vertex1)) &&
+              (vertex2.equals(((Edge) o).vertex2))) ||
+             ((vertex1.equals(((Edge) o).vertex2)) &&
+              (vertex2.equals(((Edge) o).vertex1)));
+    } else {
+      return false;
+    }
+  }
+}
